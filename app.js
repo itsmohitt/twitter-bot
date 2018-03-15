7b18cf2f05d9dfb155d88f56c3dcd0b6a999eabb
Mohit Gupta <itsmohitt@gmail.com>
2018-03-15 20:29:01 +0530

    Added Twitter bots code

--- app.js
+++ app.js
@@ -18,7 +18,7 @@ app.set('view engine', 'hbs');
 //app.use(favicon(path.join(__dirname, 'public', 'favicon.ico')));
 app.use(logger('dev'));
 app.use(bodyParser.json());
-app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.urlencoded({extended: false}));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
@@ -26,48 +26,39 @@ app.use('/', index);
 app.use('/users', users);
 
 // catch 404 and forward to error handler
-app.use(function(req, res, next) {
-  var err = new Error('Not Found');
-  err.status = 404;
-  next(err);
+app.use(function (req, res, next) {
+    var err = new Error('Not Found');
+    err.status = 404;
+    next(err);
 });
 
 // error handler
-app.use(function(err, req, res, next) {
-  // set locals, only providing error in development
-  res.locals.message = err.message;
-  res.locals.error = req.app.get('env') === 'development' ? err : {};
+app.use(function (err, req, res, next) {
+    // set locals, only providing error in development
+    res.locals.message = err.message;
+    res.locals.error = req.app.get('env') === 'development' ? err : {};
 
-  // render the error page
-  res.status(err.status || 500);
-  res.render('error');
+    // render the error page
+    res.status(err.status || 500);
+    res.render('error');
 });
 
-
-
-
-
-
-    // Tweets Data is being stored in mongo
-    var client = new Twitter({
-        consumer_key: 'yOTtNuIyaEBGT825culpz6ZQ8',
-        consumer_secret: 'lNahQ7OXdn4yoKG2eSLxbOtDcipg9IJ9YBdKeysBbiErdk2uzg',
-        access_token_key: '3772433354-EwlnWXPKJGAoSwefwrIA7jVPNppXgV1bzHFUyr2',
-        access_token_secret: 'g6j4OlJG5HaKSkD87WDjvUM3GBWJWbAM8sGWHnn67CwAn'
+console.log("Twitter Client code starts");
+// Tweets Data is being stored in mongo
+var client = new Twitter({
+    consumer_key: 'yOTtNuIyaEBGT825culpz6ZQ8',
+    consumer_secret: 'lNahQ7OXdn4yoKG2eSLxbOtDcipg9IJ9YBdKeysBbiErdk2uzg',
+    access_token_key: '3772433354-EwlnWXPKJGAoSwefwrIA7jVPNppXgV1bzHFUyr2',
+    access_token_secret: 'g6j4OlJG5HaKSkD87WDjvUM3GBWJWbAM8sGWHnn67CwAn'
+});
+client.stream('statuses/filter', {track: 'javascript'}, function(stream) {
+    stream.on('data', function(event) {
+        console.log(event && event.text);
     });
-    client.stream('statuses/filter', {language:'en',follow:'3772433354'}, function(stream) {
-        stream.on('data', function(event) {
-            if(!event.retweeted_status) {
-                console.log(event);
-            }else{
-             // console.log("Retweet "+ event.text);
-            }
-        });
-        stream.on('error', function(error) {
-            console.log(error);
-        });
+    stream.on('error', function(error) {
+        throw error;
     });
-
+});
 
 
 module.exports = app;
